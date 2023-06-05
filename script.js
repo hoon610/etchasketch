7a054c4339bf0da4a0fc14c4cf2488b0b68335c3
hoon610 <bhoonkang@gmail.com>
2023-06-05 15:30:00 -0400

    Add ability to redraw the grid

--- script.js
+++ script.js
@@ -2,33 +2,50 @@ let dimensions = 100;
 const container = document.getElementById("container");
 let color = "black";
 
-for (i = dimensions; i > 0; i--){
-    let row = document.createElement("div");
-    container.appendChild(row).className = "gridRow";
-}
-const gridRows = document.getElementsByClassName("gridRow");
-for (let i = 0; i < gridRows.length; i++) {
-    const gridRow = gridRows[i];
-
-    gridRow.style.height = `${800 / dimensions}px`;
-    gridRow.style.minWidth = "800px";
-    gridRow.style.display = "flex";
-    for (let j = dimensions; j > 0; j--){
-        let pixel = document.createElement("div");
-        pixel.style.height = `${800 / dimensions}px`;
-        pixel.style.width = `${800 / dimensions}px`;
-        gridRow.appendChild(pixel).className = "pixel";
+function draw() {
+    for (i = dimensions; i > 0; i--) {
+        let row = document.createElement("div");
+        container.appendChild(row).className = "gridRow";
     }
+    const gridRows = document.getElementsByClassName("gridRow");
+    for (let i = 0; i < gridRows.length; i++) {
+        const gridRow = gridRows[i];
 
-  }
-  const pixels = document.getElementsByClassName("pixel");
-  
-  for (let i = 0; i < pixels.length; i++) {
-    pixels[i].addEventListener("mouseover", colorOver);
-  }
-  function colorOver(event) {  
-        event.target.style.backgroundColor = String(color);
-   };
+        gridRow.style.height = `${800 / dimensions}px`;
+        gridRow.style.minWidth = "800px";
+        gridRow.style.display = "flex";
+        for (let j = dimensions; j > 0; j--) {
+            let pixel = document.createElement("div");
+            pixel.style.height = `${800 / dimensions}px`;
+            pixel.style.width = `${800 / dimensions}px`;
+            gridRow.appendChild(pixel).className = "pixel";
+        }
+
+    }
+    const pixels = document.getElementsByClassName("pixel");
+
+    for (let i = 0; i < pixels.length; i++) {
+        pixels[i].addEventListener("mouseover", colorOver);
+    }
+}
+draw();
+
+const resize = document.getElementById("resize");
+resize.addEventListener("click", redraw);
+
+function redraw() {
+    dimensions = parseInt(prompt("How many pizels on each side?"));
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+    draw();
+}
 
 
- 
+
+function colorOver(event) {
+    event.target.style.backgroundColor = String(color);
+};
+
+
+
