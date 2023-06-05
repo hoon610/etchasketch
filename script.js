13c8c21089f8b0476dcc1d6a25d34c39d602f24f
hoon610 <bhoonkang@gmail.com>
2023-06-05 18:08:37 -0400

    Add full functionality of buttons

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
-let dimensions = 100;
+let dimensions = 16;
 const container = document.getElementById("container");
 let color = "black";
+let rainbowStatus = false;
+const sliderValue = document.getElementById("demo");
+const slider = document.getElementById("slider");
+
+slider.addEventListener("input", handleSliderChange);
+
+function handleSliderChange(event) {
+  dimensions = event.target.value;
+  sliderValue.textContent = dimensions + " x " + dimensions;
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+}
+draw();
+}
+
 
 function draw() {
     for (i = dimensions; i > 0; i--) {
@@ -30,25 +45,23 @@ function draw() {
 }
 draw();
 
-const resize = document.getElementById("resize");
-resize.addEventListener("click", redraw);
 
-function redraw() {
-    dimensions = parseInt(prompt("How many pizels on each side?"));
-    while (container.firstChild) {
-        container.removeChild(container.firstChild);
-    }
-    draw();
-}
 
 function colorOver(event) {
+    if (rainbowStatus == true){
+        event.target.style.backgroundColor = getRandomColor()
+
+    }
+    else{
     event.target.style.backgroundColor = String(color);
+    }
 };
 
 const colorPicker = document.getElementById("colorPicker");
 colorPicker.addEventListener("input", changeColor);
 
 function changeColor(event) {
+    turnOff();
      color = event.target.value;
 
 };
@@ -64,6 +77,68 @@ for (let i = 0; i < buttons.length; i++) {
     })
 }
 
+const eraser = document.getElementById("eraser");
+eraser.addEventListener("click", function(){
+    turnOff();
+    color = "white";
+    eraser.classList.add("on");
+});
+
+const black = document.getElementById("black");
+black.addEventListener("click", function(){
+    turnOff();
+    color = "black";
+    black.classList.add("on");
+});
+
+const rainbow = document.getElementById("rainbow");
+rainbow.addEventListener("click", function(){
+    turnOff();
+    rainbow.classList.add("on");
+    rainbowStatus = true;
+
+});
+
+function getRandomColor() {
+    var red = Math.floor(Math.random() * 256);
+    var green = Math.floor(Math.random() * 256);
+    var blue = Math.floor(Math.random() * 256);
+  
+    var randomColor = "rgb(" + red + ", " + green + ", " + blue + ")";
+    return randomColor
+  }
+
+const clear = document.getElementById("clear");
+clear.addEventListener("click", function(){
+    turnOff();
+    pixels = document.getElementsByClassName("pixel");
+    for (let i = 0; i < pixels.length; i++) {
+        
+        pixels[i].style.backgroundColor = "white";
+
+    }
+    
+});
 
 
 
+
+
+
+const options = document.getElementById("options");
+function turnOff() {
+    rainbowStatus = false;
+    for (let i = 0; i < options.children.length; i++) {
+        const childElement = options.children[i];
+        childElement.classList.remove("on");
+}
+}
+
+
+const gitLink = document.getElementById("link");
+gitLink.addEventListener("mouseenter", function(){
+    this.style.transform = "scale(1.5)"
+});
+gitLink.addEventListener("mouseleave", function(){
+    this.style.transform = "scale(1.0)";
+})
